9ad13bb6058c17cb82c7d58c48f2e882a5284a1c
herdinn724 <bumaberau3@gmail.com>
2024-09-08 11:03:44 +0800

    pasang bootstrap dan bikin tabel ful tempalte bootstrap

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class EmployeeController extends Controller
 {
     public function index(){
-        return 'sukses';
+        return view('datapegawai');
     }
     
 }
